aeb362c7b3c9ea19bc36a05a7456166156991efe
Mingyu Park <alsrb4298@naver.com>
2022-03-21 00:55:20 +0900

    boj 1415

--- 사탕.cpp
+++ 사탕.cpp
@@ -1,15 +1,18 @@
-#include <bits/stdc++.h>
+#include <iostream> 
+#include <algorithm> // swap, fill
+#include <vector> 
+#include <cstring> // memset
 using namespace std;
 using ll = long long;
 
-ll dp[500005];
+ll dp[500005], dp_nxt[500005];
 bool prime[500005];
 int cnt[10004];
-vector<int> arr;
+vector<int> arr; // 사탕을 저장
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    memset(prime, true, sizeof(prime));
+    fill(prime, prime + 500005, true);
     for (int i = 2; i * i <= 500000; i++) {
         if (!prime[i]) continue;
         for (int j = i * i; j <= 500000; j += i) {
@@ -28,11 +31,14 @@ int main() {
 
     dp[0] = 1;
     for (auto cur : arr) {
-        for (int i = 500000; i >= 0; i--) {
+        for (int i = 0; i <= 500000; i++) {
+            dp_nxt[i] += dp[i];
             for (int j = 1; j <= cnt[cur]; j++) {
-                if (i - cur * j >= 0) dp[i] += dp[i - cur * j];
+                if (i - cur * j >= 0) dp_nxt[i] += dp[i - cur * j];
             }
         }
+        swap(dp, dp_nxt);
+        memset(dp_nxt, 0, sizeof(dp_nxt));
     }
 
     ll ans = 0;
