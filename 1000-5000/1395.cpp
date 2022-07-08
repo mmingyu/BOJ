770c3832ef6bc591eb2eb0d8a36e792d1dcd6eb9
Mingyu Park <alsrb4298@naver.com>
2022-07-08 17:24:56 +0900

    boj 스위치 - Lazy Propagation

---
+++ 1000-5000/1395.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define all(x) (x).begin(), (x).end()
+#define sz(x) int((x).size())
+
+template<class T>
+class LazySegtree {
+private:
+    const int N;
+    vector<T> tree;
+    vector<T> lazy;
+public:
+    LazySegtree(int N) : N(N) {
+        tree.resize(N << 2);
+        lazy.resize(N << 2);
+    }
+
+    void propagate(int n, int s, int e) {
+        if (lazy[n] & 1) {
+            tree[n] = (e - s + 1) - tree[n];
+            if (s != e) {
+                lazy[n << 1] += lazy[n];
+                lazy[n << 1 | 1] += lazy[n];
+            }
+        }
+        lazy[n] = 0;
+    }
+
+    void update(int qs, int qe, T diff, int n, int s, int e) {
+        propagate(n, s, e);
+        if (e < qs || s > qe) return;
+        if (qs <= s && e <= qe) {
+            tree[n] = (e - s + 1) - tree[n];
+            if (s != e) {
+                lazy[n << 1] += diff;
+                lazy[n << 1 | 1] += diff;
+            }
+            return;
+        }
+        int mid = s + e >> 1;
+        update(qs, qe, diff, n << 1, s, mid);
+        update(qs, qe, diff, n << 1 | 1, mid + 1, e);
+        tree[n] = tree[n << 1] + tree[n << 1 | 1];
+    }
+    void update(int qs, int qe, T diff) { update(qs, qe, diff, 1, 0, N); }
+
+    T query(int qs, int qe, int n, int s, int e) {
+        propagate(n, s, e);
+        if (e < qs || s > qe) return 0;
+        if (qs <= s && e <= qe) return tree[n];
+        int mid = s + e >> 1;
+        return query(qs, qe, n << 1, s, mid) + query(qs, qe, n << 1 | 1, mid + 1, e);
+    }
+    T query(int qs, int qe) { return query(qs, qe, 1, 0, N); }
+};
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int n, m; cin >> n >> m; 
+    LazySegtree<ll> seg(n + 1);
+    while (m--) {
+        int q, s, e; cin >> q >> s >> e;
+        if (q == 0) seg.update(s, e, 1);
+        if (q == 1) cout << seg.query(s, e) << "\n";
+    }
+}
