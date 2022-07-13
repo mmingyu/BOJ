2dab9815dd1514d1bbb029f2bbd7e57d93b5dc6e
Mingyu Park <alsrb4298@naver.com>
2022-07-13 21:50:45 +0900

    boj 코딩은 체육과목 입니다 - 구현

---
+++ 10001-30000/25314.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define all(x) (x).begin(), (x).end()
+#define sz(x) int((x).size())
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int n; cin >> n;
+    n /= 4;
+    while (n--) cout << "long ";
+    cout << "int";
+}
