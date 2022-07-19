f9e4fdfdee7bb120859ebcf7cddc393b27d8f63e
Mingyu Park <alsrb4298@naver.com>
2022-07-19 14:58:40 +0900

    boj 수열과 쿼리 6 - mo's

---
+++ 10001-30000/13548.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define all(x) (x).begin(), (x).end()
+#define sz(x) int((x).size())
+
+
+constexpr int N = 1e5 + 5;
+struct Q { int s, e, i; } q[N];
+int a[N], ans[N], cnt[N], ccnt[N], sqn, x;
+
+void pl(int i) {
+    int &cc = cnt[a[i]];
+    ccnt[cc]--;
+    cc++;
+    ccnt[cc]++;
+    x = max(cc, x);
+}
+
+void mi(int i) {
+    int &cc = cnt[a[i]];
+    if (--ccnt[cc] == 0 && cc == x) x--;
+    cc--;
+    ccnt[cc]++;
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int n; cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    int m; cin >> m;
+    for (int i = 0; i < m; i++) {
+        int s, e; cin >> s >> e;
+        q[i] = {s, e, i};
+    }
+    
+    sqn = sqrt(n);
+    sort(q, q + m, [&](const Q &a, const Q &b) {
+        if (a.s / sqn != b.s / sqn) return a.s < b.s;
+        return a.e < b.e;
+    });
+
+    int s = q[0].s, e = q[0].e;
+    for (int i = s; i <= e; i++) pl(i);
+    ans[q[0].i] = x;
+
+    for (int i = 1; i < m; i++) {
+        const auto &[qs, qe, qi] = q[i];
+        while (s < qs) mi(s++); 
+        while (e < qe) pl(++e);
+        while (qs < s) pl(--s); 
+        while (qe < e) mi(e--);
+        ans[qi] = x;
+    }
+
+    for (int i = 0; i < m; i++) cout << ans[i] << "\n";
+}
