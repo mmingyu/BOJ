7248380bd465cc1e8391f10cd08b1ed9e0cca8a1
Mingyu Park <alsrb4298@naver.com>
2022-10-10 16:04:57 +0900

    boj 재귀의 귀재

---
+++ 10001-30000/25501.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int cnt;
+
+int recursion(const char *s, int l, int r){
+    cnt++;
+    if(l >= r) return 1;
+    else if(s[l] != s[r]) return 0;
+    else return recursion(s, l+1, r-1);
+}
+
+int isPalindrome(const char *s){
+    return recursion(s, 0, strlen(s)-1);
+}
+
+char s[1003];
+
+int main(){
+    int n; scanf("%d", &n);
+    while (n--) {
+        scanf("%s", s);
+        cnt = 0;
+        int ans = isPalindrome(s);
+        printf("%d %d\n", ans, cnt);   
+    }
+}
