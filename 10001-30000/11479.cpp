d8c5b6a2592d3cdc131b5537ca8b00b2fba256f0
Mingyu Park <alsrb4298@naver.com>
2022-10-11 17:35:41 +0900

    boj 서로 다른 부분 문자열의 개수 2 - LCP

---
+++ 10001-30000/11479.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define all(x) (x).begin(), (x).end()
+#define sz(x) int((x).size())
+
+struct LCP {
+    vector<int> sa, pos, tmp, lcp;
+    string s;
+    int n;
+
+    void getSuffixArray() {
+        for (int i = 0; i < n; i++) sa[i] = i, pos[i] = s[i];
+        for (int k = 1; ; k <<= 1) {
+            auto cmp = [&](int a, int b) -> bool {
+                if (pos[a] != pos[b]) return pos[a] < pos[b];
+                if (a + k < n && b + k < n) return pos[a + k] < pos[b + k];
+                return a > b;
+            };
+            stable_sort(sa.begin(), sa.end(), cmp);
+            for (int i = 1; i < n; i++) tmp[i] = tmp[i-1] + cmp(sa[i-1], sa[i]);
+            for (int i = 0; i < n; i++) pos[sa[i]] = tmp[i];
+            if (tmp.back() + 1 == n) break;
+        }
+    }
+
+    // O(n)
+    void getLCP() {
+        for (int i = 0, j = 0; i < n; i++, j = max(j-1, 0)) {
+            if (pos[i] == 0) continue;
+            while (sa[pos[i]-1] + j < n && sa[pos[i]] + j < n 
+                && s[sa[pos[i]-1] + j] == s[sa[pos[i]] + j]) j++;
+            lcp[pos[i]] = j;
+        }
+    }
+
+    void build(const string &_s) {
+        s = _s;
+        n = s.size();
+        sa.resize(n); pos.resize(n); tmp.resize(n); lcp.resize(n);
+        getSuffixArray();
+        getLCP();
+    }
+};
+
+LCP suf;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    string s; cin >> s;
+    suf.build(s);
+    ll ans = ll(s.size()) * ll(s.size() + 1) / 2;
+    for (auto x : suf.lcp) ans -= x;
+    cout << ans;
+}
